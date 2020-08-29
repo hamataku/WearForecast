6e6d69fdb99c52d78cd15a46aedabee18707c6f7
hamataku <hamataku07130713@gmail.com>
2020-08-29 17:15:06 +0000

    h

--- js/photoregister.js
+++ js/photoregister.js
@@ -1,96 +1,110 @@
-$(function() {
-  var file = null; // 選択されるファイル
-  var blob = null; // 画像(BLOBデータ)
-  const THUMBNAIL_WIDTH = 500; // 画像リサイズ後の横の長さの最大値
-  const THUMBNAIL_HEIGHT = 500; // 画像リサイズ後の縦の長さの最大値
-
-  // ファイルが選択されたら
-  $('input[type=file]').change(function() {
-
-    // ファイルを取得
-    file = $(this).prop('files')[0];
-    // 選択されたファイルが画像かどうか判定
-    if (file.type != 'image/jpeg' && file.type != 'image/png') {
-      // 画像でない場合は終了
-      file = null;
-      blob = null;
-      return;
-    }
-
-    // 画像をリサイズする
-    var image = new Image();
+let count = 0;
+$('#upfile').change(function() {
+  if (this.files.length > 0) {
+    // 選択されたファイル情報を取得
+    var file = this.files[0];
+    // readerのresultプロパティに、データURLとしてエンコードされたファイルデータを格納
     var reader = new FileReader();
-    reader.onload = function(e) {
-      image.onload = function() {
-        var width, height;
-        if(image.width > image.height){
-          // 横長の画像は横のサイズを指定値にあわせる
-          var ratio = image.height/image.width;
-          width = THUMBNAIL_WIDTH;
-          height = THUMBNAIL_WIDTH * ratio;
-        } else {
-          // 縦長の画像は縦のサイズを指定値にあわせる
-          var ratio = image.width/image.height;
-          width = THUMBNAIL_HEIGHT * ratio;
-          height = THUMBNAIL_HEIGHT;
-        }
-        // サムネ描画用canvasのサイズを上で算出した値に変更
-        var canvas = $('#canvas')
-                     .attr('width', width)
-                     .attr('height', height);
-        var ctx = canvas[0].getContext('2d');
-        // canvasに既に描画されている画像をクリア
-        ctx.clearRect(0,0,width,height);
-        // canvasにサムネイルを描画
-        ctx.drawImage(image,0,0,image.width,image.height,0,0,width,height);
-
-        // canvasからbase64画像データを取得
-        var base64 = canvas.get(0).toDataURL('image/jpeg');        
-        // base64からBlobデータを作成
-        var barr, bin, i, len;
-        bin = atob(base64.split('base64,')[1]);
-        len = bin.length;
-        barr = new Uint8Array(len);
-        i = 0;
-        while (i < len) {
-          barr[i] = bin.charCodeAt(i);
-          i++;
-        }
-        blob = new Blob([barr], {type: 'image/jpeg'});
-        console.log(blob);
-      }
-      image.src = e.target.result;
-    }
     reader.readAsDataURL(file);
-  });
-
-
-  // アップロード開始ボタンがクリックされたら
-  $('#upload').click(function(){
-
-    // ファイルが指定されていなければ何も起こらない
-    if(!file || !blob) {
-      return;
+    reader.onload = function() {
+      $('#thumbnail').attr('src', reader.result);
     }
+  }
+});
 
-    var name, fd = new FormData();
-    fd.append('file', blob); // ファイルを添付する
+// 画像をアップロード
+$('#imgForm').on('submit', function(e) {
+  
+  user = "Hello";
+  $('#upfile').attr({
+    'name': user+"/"+count
+  });
+  count++;
 
-    $.ajax({
-      url: "https://d4z4c4pde2.execute-api.ap-northeast-1.amazonaws.com/prod", // 送信先
-      type: 'POST',
-      dataType: 'json',
-      data: fd,
-      processData: false,
-      contentType: false
-    })
-    .done(function( data, textStatus, jqXHR ) {
-      // 送信成功
-    })
-    .fail(function( jqXHR, textStatus, errorThrown ) {
-      // 送信失敗
-    });  
+  e.preventDefault();
+  var formData = new FormData($('#imgForm').get(0));
+  
+  $.ajax($(this).attr('action'), {
+    type: 'post',
+    processData: false,
+    contentType: false,
+    data: formData,
+    success: document.getElementById('compUpload').innerHTML = 'アップロード中' // 送信に成功したとき
 
+  }).done(function(response) {
+    let jsonbody = JSON.parse(response.body);
+    console.log('succes!'); // レスポンスがあったとき
+    //ローカルストレージにレスポンスのファイル名を格納
+    var array = [];
+    var obj = {
+      'キー1': '値1',
+      'キー2': '値2'
+    };
+    array.push(obj);
+    var setjson = JSON.stringify(obj);
+    localStorage.setItem('キー', jsonbody.message);
+    document.getElementById('compUpload').innerHTML = 'アップロード完了しました。写真に写っているのは'+jsonbody.message+'です。'
+
+  }).fail(function() {
+    console.log('error!'); // エラーが発生したとき
+  });
+  return false;
+});
+
+(() => {
+  // ユーザープールの設定
+  const poolData = {
+    UserPoolId: "ap-northeast-1_bX1jgrpZQ",
+    ClientId: "63ubgk2st9c0r6m7h49hb77q2c"
+  };
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  const cognitoUser = userPool.getCurrentUser(); // 現在のユーザー
+
+  const currentUserData = {}; // ユーザーの属性情報
+
+  // Amazon Cognito 認証情報プロバイダーを初期化します
+  AWS.config.region = "ap-northeast-1"; // リージョン
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: "ap-northeast-1:1d64aa86-ba4d-4ce0-9b8d-a8977e8bc43e"
   });
 
-});
+  // 現在のユーザーの属性情報を取得・表示する
+  // 現在のユーザー情報が取得できているか？
+  if (cognitoUser != null) {
+    cognitoUser.getSession((err, session) => {
+      if (err) {
+        console.log(err);
+        location.href = "signin.html";
+      } else {
+        // ユーザの属性を取得
+        cognitoUser.getUserAttributes((err, result) => {
+          if (err) {
+            location.href = "signin.html";
+          }
+
+          // 取得した属性情報を連想配列に格納
+          for (i = 0; i < result.length; i++) {
+            currentUserData[result[i].getName()] = result[i].getValue();
+          }
+          document.getElementById("name").innerHTML =
+            "ようこそ！" + currentUserData["name"] + "さん";
+          document.getElementById("role").innerHTML =
+            "Your Role is " + currentUserData["custom:custom:role"];
+          document.getElementById("email").innerHTML =
+            "Your E-Mail is " + currentUserData["email"];
+
+          // サインアウト処理
+          const signoutButton = document.getElementById("signout");
+          signoutButton.addEventListener("click", event => {
+            cognitoUser.signOut();
+            location.reload();
+          });
+          signoutButton.hidden = false;
+          console.log(currentUserData);
+        });
+      }
+    });
+  } else {
+    location.href = "signin.html";
+  }
+})();
