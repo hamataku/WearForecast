ca8b5fa4824bd73394a8d8a8af1dceb367a315ba
hamataku <hamataku07130713@gmail.com>
2020-08-31 04:01:46 +0000

    h

--- js/forecast.js
+++ js/forecast.js
@@ -1,4 +1,9 @@
 var email = "";
+
+function hexToBase64(str) {
+    return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
+}
+
 (() => {
     // ユーザープールの設定
     const poolData = {
@@ -8,7 +13,7 @@ var email = "";
     const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
     const cognitoUser = userPool.getCurrentUser(); // 現在のユーザー
 
-    var currentUserData = {}; // ユーザーの属性情報
+    const currentUserData = {}; // ユーザーの属性情報
 
     // Amazon Cognito 認証情報プロバイダーを初期化します
     AWS.config.region = "ap-northeast-1"; // リージョン
@@ -34,13 +39,9 @@ var email = "";
                     for (i = 0; i < result.length; i++) {
                         currentUserData[result[i].getName()] = result[i].getValue();
                     }
-                    document.getElementById("name").innerHTML =
-                        "ようこそ！" + currentUserData["name"] + "さん";
-                    document.getElementById("role").innerHTML =
-                        "Your Role is " + currentUserData["custom:custom:role"];
-                    document.getElementById("email").innerHTML =
-                        "Your E-Mail is " + currentUserData["email"];
-                        email = currentUserData["email"];
+                    //document.getElementById("name").innerHTML ="ようこそ！" + currentUserData["name"] + "さん";
+                    //document.getElementById("role").innerHTML ="Your Role is " + currentUserData["custom:custom:role"];
+                    //document.getElementById("email").innerHTML ="Your E-Mail is " + currentUserData["email"];
 
                     // サインアウト処理
                     const signoutButton = document.getElementById("signout");
@@ -49,90 +50,118 @@ var email = "";
                         location.reload();
                     });
                     signoutButton.hidden = false;
+                    email = currentUserData["email"];
                     console.log(currentUserData);
+
+                    //現在位置の取得
+                    // Geolocation APIに対応している
+                    if (navigator.geolocation) {
+                        // 現在地を取得
+                        navigator.geolocation.getCurrentPosition(
+                            // [第1引数] 取得に成功した場合の関数
+                            function (position) {
+                                // 取得したデータの整理
+                                var data = position.coords;
+                                // データの整理
+                                var lat = data.latitude; //緯度
+                                var lng = data.longitude;　//軽度
+                                var formData = new FormData();
+                                formData.append("lat", lat);
+                                formData.append("lng", lng);
+                                console.log(email);
+                                console.log(currentUserData["email"],lat,lng);
+                                var hash = CybozuLabs.MD5.calc(email);
+                                formData.append("hash", hash);
+
+                                $.ajax("https://cb4rvlwdl3.execute-api.ap-northeast-1.amazonaws.com/default/savepicture", {
+                                    type: 'post',
+                                    processData: false,
+                                    contentType: false,
+                                    data: formData,
+                                    success: console.log("success!")
+                                }).done(function (response) {
+                                    let jsonbody = JSON.parse(response.body);
+                                    console.log(jsonbody);
+                                    const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+                                        const byteCharacters = atob(b64Data);
+                                        const byteArrays = [];
+
+                                        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                                            const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                                            const byteNumbers = new Array(slice.length);
+                                            for (let i = 0; i < slice.length; i++) {
+                                                byteNumbers[i] = slice.charCodeAt(i);
+                                            }
+
+                                            const byteArray = new Uint8Array(byteNumbers);
+                                            byteArrays.Push(byteArray);
+                                        }
+
+                                        const blob = new Blob(byteArrays, {type: contentType});
+                                        return blob;
+                                    }
+                                    count = 0;
+                                    for (var k in jsonbody) {
+                                        console.log("項目："+k);
+                                        var d = jsonbody[k];
+
+                                        $("#"+count+"0").attr('src',d["tops"]);
+                                        $("#"+count+"1").attr('src',d["bottoms"]);
+                                        $("#"+count+"2").attr('src',d["shoes"]);
+                                        $("#"+count+"i").attr('src',"http://openweathermap.org/img/w/"+d["icon"]+".png");
+                                        $("#tmp"+count).html(parseInt(d["tmp"], 10)+"°C");
+                                        $("#wea"+count).html(d["weather"]);
+                                        $("#view_time"+count).html(d["day"]);
+
+                                        count++;
+                                    }
+
+
+
+                                }).fail(function () {
+                                    console.log('error!'); // エラーが発生したとき
+                                });
+                            },
+
+                            // [第2引数] 取得に失敗した場合の関数
+                            function (error) {
+
+                                // エラー番号に対応したメッセージ
+                                var errorInfo = [
+                                    "原因不明のエラーが発生しました…。",
+                                    "位置情報の取得が許可されませんでした…。",
+                                    "電波状況などで位置情報が取得できませんでした…。",
+                                    "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
+                                ];
+
+                                // エラー番号
+                                var errorNo = error.code;
+
+                                // エラーメッセージ
+                                var errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[errorNo];
+
+                                // アラート表示
+                                alert(errorMessage);
+                            },
+                            // [第3引数] オプション
+                            {
+                                "enableHighAccuracy": false,
+                                "timeout": 8000,
+                                "maximumAge": 2000,
+                            }
+                        );
+                    } else {
+                        // 現在位置を取得できない場合の処理
+                        alert("あなたの端末では、現在位置を取得できません。");
+                    }
                 });
             }
+
         });
 
-        //現在位置の取得
-        // Geolocation APIに対応している
-        if (navigator.geolocation){
-            // 現在地を取得
-            navigator.geolocation.getCurrentPosition(
-                // [第1引数] 取得に成功した場合の関数
-                function( position)
-                {
-                    // 取得したデータの整理
-                    var data = position.coords ;
-                    // データの整理
-                    var lat = data.latitude ; //緯度
-                    var lng = data.longitude ;　//軽度
-                    var formData = new FormData();
-                    formData.append("lat",lat);
-                    formData.append("lng",lng);
-                    console.log(email);
-                    var hash = CybozuLabs.MD5.calc(email);
-                    formData.append("hash",hash);
-
-                    $.ajax("https://cb4rvlwdl3.execute-api.ap-northeast-1.amazonaws.com/default/savepicture", {
-                        type: 'post',
-                        processData: false,
-                        contentType: false,
-                        data: formData,
-                        success: document.getElementById('compUpload').innerHTML = '送信中' // 送信に成功したとき
-                    }).done(function(response) {
-                        let jsonbody = JSON.parse(response.body);
-                        console.log('succes!'); // レスポンスがあったとき
-                        //ローカルストレージにレスポンスのファイル名を格納
-                        var array = [];
-                        var obj = {
-                            'キー1': '値1',
-                            'キー2': '値2'
-                        };
-                        array.push(obj);
-                        var setjson = JSON.stringify(obj);
-                        localStorage.setItem('キー', count);
-
-                    }).fail(function() {
-                        console.log('error!'); // エラーが発生したとき
-                    });
-
-                },
-
-                // [第2引数] 取得に失敗した場合の関数
-                function( error )
-                {
-
-                    // エラー番号に対応したメッセージ
-                    var errorInfo = [
-                        "原因不明のエラーが発生しました…。" ,
-                        "位置情報の取得が許可されませんでした…。" ,
-                        "電波状況などで位置情報が取得できませんでした…。" ,
-                        "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
-                    ] ;
-
-                    // エラー番号
-                    var errorNo = error.code ;
-
-                    // エラーメッセージ
-                    var errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[ errorNo ] ;
-
-                    // アラート表示
-                    alert( errorMessage ) ;
-                } ,
-                // [第3引数] オプション
-                {
-                    "enableHighAccuracy": false,
-                    "timeout": 8000,
-                    "maximumAge": 2000,
-                }
-
-            ) ;
-        }else{
-            // 現在位置を取得できない場合の処理
-            alert( "あなたの端末では、現在位置を取得できません。" ) ;
-        }
     } else {
         location.href = "signin.html";
     }
 })();
+
